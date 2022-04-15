a6c45e0f6f6e0d28b584256e4989faa2f6752a09
Fabrício Alencar <fabricio.alencar@gmail.com>
2022-04-15 11:24:09 -0300

    Aprendendo clone

---
+++ src/Aula_logica/Teste_Clone_GitHub.java
@@ -0,0 +1,10 @@
+package Aula_logica;
+
+public class Teste_Clone_GitHub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Teste 1!");
+	}
+
+}
